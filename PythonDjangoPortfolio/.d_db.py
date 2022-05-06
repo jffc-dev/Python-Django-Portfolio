3037bd70713bcc26232fec917483ee28e41a4847
Javier Flores <jffc.dev@gmail.com>
2022-05-05 21:20:07 -0500

    feat(settings): Settings for develop added

---
+++ PythonDjangoPortfolio/.d_db.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SQLITE = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio_db',
+        'USER': 'portfolio_user',
+        'PASSWORD': 'p0rtf0lioJFFC',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
